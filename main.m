d2377602f45dfc8d91821c3159fdfb2084c3ad05
Alex Morega <alex@grep.ro>
2010-05-22 09:12:00 +0300

    initial commit - blank xcode project

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  tinymail
+//
+//  Created by Alex Morega on 22/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
---
+++ tinymailAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  tinymailAppDelegate.m
+//  tinymail
+//
+//  Created by Alex Morega on 22/5/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "tinymailAppDelegate.h"
+
+@implementation tinymailAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
