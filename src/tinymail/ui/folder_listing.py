7d1286657b6e47ca0ae220db75367a37bb0e884c
Alex Morega <alex@grep.ro>
2010-05-23 13:35:03 +0300

    react to selection; fix name of arg

--- src/tinymail/ui/folder_listing.py
+++ src/tinymail/ui/folder_listing.py
@@ -21,7 +21,7 @@ class FolderListingDataSource(NSObject):
         assert item is None
         return len(self.items)
 
-    def outlineView_isItemExpandable_(self, outlineView, item):
+    def outlineView_isItemExpandable_(self, outline_view, item):
         #print "outlineView_isItemExpandable_(%r) called" % item
         assert isinstance(item, FolderListingItem)
         return False
@@ -34,18 +34,26 @@ class FolderListingDataSource(NSObject):
         #print 'returning %r' % output
         return output
 
-    def outlineView_objectValueForTableColumn_byItem_(self, outlineView,
+    def outlineView_objectValueForTableColumn_byItem_(self, outline_view,
                                                       tableColumn, item):
         #print ("outlineView_objectValueForTableColumn_byItem_(%r, %r) called"
         #       % (tableColumn, item))
         assert isinstance(item, FolderListingItem)
         return item.value
 
-    def outlineView_shouldEditTableColumn_item_(self, outlineView,
+    def outlineView_shouldEditTableColumn_item_(self, outline_view,
                                                 tableColumn, item):
         #print ("outlineView_shouldEditTableColumn_item_(%r, %r) called"
         #       % (tableColumn, item))
         return False
+
+    def outlineViewSelectionDidChange_(self, notification):
+        outline_view = notification.object()
+        row = outline_view.selectedRow()
+        if row == -1:
+            print "[empty selection]"
+        else:
+            print outline_view.itemAtRow_(row).value
 
 def set_up_folder_listing(imap_conn, folders_tree):
     from tinymail.ui.folder_listing import FolderListingDataSource
