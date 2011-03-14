7b86c82ae739e97230816cbd260fcdbc27a5ffc4
Alex Morega <alex@grep.ro>
2011-03-13 22:54:17 -0400

    workers in threads

--- src/awesomail/asynch.py
+++ src/awesomail/asynch.py
@@ -1,3 +1,5 @@
+import Queue
+import threading
 import logging
 import monocle
 
@@ -17,3 +19,38 @@ class AsynchJob(object):
     @monocle.o
     def do_stuff(self):
         pass
+
+def call_on_main_thread(callback, result):
+    raise NotImplementedError
+
+def worker_loop(in_queue, worker):
+    while True:
+        msg = in_queue.get()
+        if msg is None:
+            return
+        callback, method, args, kwargs = msg
+        result = getattr(worker, method)(*args, **kwargs)
+        call_on_main_thread(callback, result)
+
+class AsynchWorkerProxy(object):
+    def __init__(self, in_queue, thread):
+        self._in_queue = in_queue
+        self._thread = thread
+
+    def __getattr__(self, name):
+        @monocle.o
+        def method_proxy(*args, **kwargs):
+            callback = monocle.callback.Callback()
+            self._in_queue.put((callback, name, args, kwargs))
+            return callback
+        return method_proxy
+
+    def done(self):
+        self._in_queue.put(None)
+        self._thread.join()
+
+def start_worker(worker):
+    in_queue = Queue.Queue()
+    thread = threading.Thread(target=worker_loop, args=(in_queue, worker))
+    thread.start()
+    return AsynchWorkerProxy(in_queue, thread)
--- src/awesomail/tests/test_asynch.py
+++ src/awesomail/tests/test_asynch.py
@@ -1,5 +1,6 @@
 import unittest
 import monocle
+from mock import patch
 
 @monocle.o
 def add_one(value):
@@ -29,3 +30,44 @@ class AsynchJobTest(unittest.TestCase):
         job.start()
         self.assertTrue(isinstance(job.failure, ValueError))
         self.assertEqual(job.failure.message, 'ha!')
+
+class AsynchWorkerTest(unittest.TestCase):
+    def setUp(self):
+        from awesomail import asynch
+        self._patch = patch.object(asynch, 'call_on_main_thread')
+        m = self._patch.start()
+        m.side_effect = lambda callback, result: callback(result)
+
+    def tearDown(self):
+        self._patch.stop()
+
+    def test_input_queue(self):
+        called = []
+        from awesomail.asynch import start_worker
+        class MyWorker(object):
+            def plus(self, a, b):
+                called.append((a,b))
+
+        try:
+            worker = start_worker(MyWorker())
+            d = worker.plus(3, 4)
+        finally:
+            worker.done()
+
+        self.assertEqual(called, [(3, 4)])
+
+    def test_return_value(self):
+        from awesomail.asynch import start_worker
+        class MyWorker(object):
+            def get_13(self):
+                return 13
+
+        try:
+            worker = start_worker(MyWorker())
+            d = worker.get_13()
+        finally:
+            worker.done()
+
+        self.assertEqual(d.result, 13)
+
+    # TODO test handling of exceptions from worker (incl. missing methods)
