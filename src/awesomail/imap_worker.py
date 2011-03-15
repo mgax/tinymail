b4785c9ab84b50ce60896c9690733efc64aa81a4
Alex Morega <alex@grep.ro>
2011-03-15 07:52:18 -0400

    fetch message flags in imap folder

--- src/awesomail/imap_worker.py
+++ src/awesomail/imap_worker.py
@@ -3,6 +3,12 @@ import re
 list_pattern = re.compile(r'^\((?P<flags>[^\)]*)\) '
                           r'"(?P<delim>[^"]*)" '
                           r'(?P<name>.*)$')
+
+status_pattern = re.compile(r'^(?P<name>[^(]+)\((?P<status>[\w\d\s]*)\)$')
+
+searchuid_pattern = re.compile(r'(?P<index>\d+)\s+\(UID\s*(?P<uid>\d+)'
+                               r'\s+FLAGS \((?P<flags>[^\)]*)\)\)$')
+
 class ImapWorker(object):
     def get_mailbox_names(self):
         status, entries = self.conn.list()
@@ -18,7 +24,34 @@ class ImapWorker(object):
         return paths
 
     def get_messages_in_folder(self, folder_name):
-        raise NotImplementedError
+        status, count = self.conn.select(folder_name, readonly=True)
+        assert status == 'OK'
+
+        status, data = self.conn.status(folder_name,
+                                        '(MESSAGES UIDNEXT UIDVALIDITY)')
+        assert status == 'OK'
+
+        m = status_pattern.match(data[0])
+        assert m is not None
+        assert m.group('name').strip().strip('"') == folder_name
+        bits = m.group('status').strip().split()
+        mbox_status = dict(zip(bits[::2], map(int, bits[1::2])))
+
+        message_data = {}
+        if mbox_status['MESSAGES']:
+            status, data = self.conn.fetch('1:*', '(UID FLAGS)')
+            assert status == 'OK'
+
+            for item in data:
+                m = searchuid_pattern.match(item)
+                assert m is not None
+                (uid, index) = (int(m.group('uid')), int(m.group('index')))
+                message_data[uid] = {
+                    'flags': set(m.group('flags').split()),
+                    'index': index,
+                }
+
+        return mbox_status, message_data
 
     def get_message_headers(self, folder_name, msg_id):
         raise NotImplementedError
--- src/awesomail/tests/test_imap_worker.py
+++ src/awesomail/tests/test_imap_worker.py
@@ -3,10 +3,14 @@ import unittest
 from mock import Mock
 
 class ImapWorkerTest(unittest.TestCase):
-    def test_get_mailbox_names(self):
+    def _worker_with_fake_imap(self):
         from awesomail.imap_worker import ImapWorker
         worker = ImapWorker()
         imap_conn = worker.conn = Mock(spec=imaplib.IMAP4)
+        return worker, imap_conn
+
+    def test_get_mailbox_names(self):
+        worker, imap_conn = self._worker_with_fake_imap()
         imap_conn.list.return_value = ('OK', [
             r'(\HasNoChildren) "." INBOX',
             r'(\HasNoChildren) "." "fol1"',
@@ -17,3 +21,35 @@ class ImapWorkerTest(unittest.TestCase):
         names = worker.get_mailbox_names()
 
         self.assertEqual(sorted(names), ['INBOX', 'fol1', 'fol2', 'fol2.sub'])
+
+    def test_get_messages_in_folder(self):
+        worker, imap_conn = self._worker_with_fake_imap()
+        imap_conn.select.return_value = ('OK', [])
+        imap_conn.status.return_value = ('OK', [
+            '"fol1" (MESSAGES 3 RECENT 1 UIDNEXT 14 '
+                    'UIDVALIDITY 1300189203 UNSEEN 1)'])
+        imap_conn.fetch.return_value = ('OK', [
+            '1 (UID 6 FLAGS (\\Seen))',
+            '2 (UID 8 FLAGS (\\Answered \\Seen))',
+            '3 (UID 13 FLAGS ())',
+        ])
+
+        mbox_status, message_data = worker.get_messages_in_folder('fol1')
+
+        imap_conn.select.assert_called_once_with('fol1', readonly=True)
+        imap_conn.status.assert_called_once_with(
+            'fol1', '(MESSAGES UIDNEXT UIDVALIDITY)')
+        imap_conn.fetch.assert_called_once_with('1:*', '(UID FLAGS)')
+
+        self.assertEqual(mbox_status, {
+            'UIDNEXT': 14,
+            'UIDVALIDITY': 1300189203,
+            'MESSAGES': 3,
+            'UNSEEN': 1,
+            'RECENT': 1,
+        })
+        self.assertEqual(message_data, {
+            6: {'index': 1, 'flags': set([r'\Seen'])},
+            8: {'index': 2, 'flags': set([r'\Answered', r'\Seen'])},
+            13: {'index': 3, 'flags': set()},
+        })
