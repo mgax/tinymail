2269af7e78ed3d1119bfddc632d6380cdfb00883
Alex Morega <alex@grep.ro>
2011-04-08 19:47:24 +0300

    prefix helper with _ because it's not a test

--- src/tinymail/tests/test_account.py
+++ src/tinymail/tests/test_account.py
@@ -8,7 +8,7 @@ def _make_test_db():
     from tinymail.localdata import open_local_db
     return open_local_db(':memory:')
 
-def account_for_test(config=None, db=None):
+def _account_for_test(config=None, db=None):
     from tinymail.account import Account
     if config is None:
         config = {
@@ -22,7 +22,7 @@ def account_for_test(config=None, db=None):
 
 class AccountTest(unittest.TestCase):
     def test_list_folders(self):
-        account = account_for_test()
+        account = _account_for_test()
         fol1, fol2 = Mock(), Mock()
         account._folders = {'fol1': fol1, 'fol2': fol2}
 
@@ -31,7 +31,7 @@ class AccountTest(unittest.TestCase):
         self.assertEqual(folders, [fol1, fol2])
 
     def test_get_folder(self):
-        account = account_for_test()
+        account = _account_for_test()
         fol1, fol2 = Mock(), Mock()
         account._folders = {'fol1': fol1, 'fol2': fol2}
 
@@ -99,7 +99,7 @@ def mock_worker(**folders):
 class AccountUpdateTest(unittest.TestCase):
 
     def test_list_folders(self):
-        account = account_for_test()
+        account = _account_for_test()
         folders = {'fol1': {}, 'fol2': {}}
 
         with mock_worker(**folders):
@@ -112,7 +112,7 @@ class AccountUpdateTest(unittest.TestCase):
         self.assertEqual(signal_log, [account])
 
     def test_list_messages(self):
-        account = account_for_test()
+        account = _account_for_test()
 
         with mock_worker(fol1={6: None, 8: None}):
             signal_log = []
@@ -125,7 +125,7 @@ class AccountUpdateTest(unittest.TestCase):
         self.assertEqual(signal_log, [fol1])
 
     def test_message_removed_on_server(self):
-        account = account_for_test()
+        account = _account_for_test()
         with mock_worker(fol1={6: None, 8: None}):
             account.perform_update()
 
@@ -136,7 +136,7 @@ class AccountUpdateTest(unittest.TestCase):
         self.assertEqual([m.uid for m in fol1.list_messages()], [6])
 
     def test_only_get_new_headers(self):
-        account = account_for_test()
+        account = _account_for_test()
         with mock_worker(fol1={6: None, 8: None}):
             account.perform_update()
 
@@ -146,7 +146,7 @@ class AccountUpdateTest(unittest.TestCase):
             worker.get_message_headers.assert_called_once_with(set([_index]))
 
     def test_empty_folder(self):
-        account = account_for_test()
+        account = _account_for_test()
 
         with mock_worker(fol1={}) as worker:
             signal_log = []
@@ -155,7 +155,7 @@ class AccountUpdateTest(unittest.TestCase):
         self.assertFalse(worker.get_message_headers.called)
 
     def test_load_full_message(self):
-        account = account_for_test()
+        account = _account_for_test()
         mime_message = "Subject: hi\r\n\r\nHello world!"
 
         with mock_worker(fol1={6: None}) as worker:
@@ -170,7 +170,7 @@ class AccountUpdateTest(unittest.TestCase):
         self.assertEqual(signal_log, [message])
 
     def test_folder_removed_on_server(self):
-        account = account_for_test()
+        account = _account_for_test()
 
         with mock_worker(fol1={}, fol2={}):
             account.perform_update()
@@ -181,7 +181,7 @@ class AccountUpdateTest(unittest.TestCase):
         self.assertEqual([f.name for f in account.list_folders()], ['fol1'])
 
     def test_trust_uidvalidity(self):
-        account = account_for_test()
+        account = _account_for_test()
         msg13_data = (13, set([r'\Seen']), "Subject: test message")
         msg13_bis_data = (13, set([r'\Seen']), "Subject: another message")
         with mock_worker(fol1={13: msg13_data}):
@@ -195,7 +195,7 @@ class AccountUpdateTest(unittest.TestCase):
                          [msg13_data[2]])
 
     def test_uidvalidity_changed(self):
-        account = account_for_test()
+        account = _account_for_test()
         msg13_data = (13, set([r'\Seen']), "Subject: test message")
         msg13_bis_data = (13, set([r'\Seen']), "Subject: another message")
         with mock_worker(fol1={13: msg13_data, 'UIDVALIDITY': 1234}):
@@ -211,19 +211,19 @@ class AccountUpdateTest(unittest.TestCase):
 class PersistenceTest(unittest.TestCase):
     def test_folders(self):
         db = _make_test_db()
-        account = account_for_test(db=db)
+        account = _account_for_test(db=db)
 
         with mock_worker(myfolder={}) as worker:
             account.perform_update()
 
-        account2 = account_for_test(db=db)
+        account2 = _account_for_test(db=db)
         folders = list(account2.list_folders())
         self.assertEqual(len(folders), 1)
         self.assertEqual(folders[0].name, 'myfolder')
 
     def test_folders_removed(self):
         db = _make_test_db()
-        account = account_for_test(db=db)
+        account = _account_for_test(db=db)
 
         with mock_worker(fol1={}, fol2={}):
             account.perform_update()
@@ -231,12 +231,12 @@ class PersistenceTest(unittest.TestCase):
         with mock_worker(fol1={}):
             account.perform_update()
 
-        account2 = account_for_test(db=db)
+        account2 = _account_for_test(db=db)
         self.assertEqual([f.name for f in account2.list_folders()], ['fol1'])
 
     def test_messages(self):
         db = _make_test_db()
-        account = account_for_test(db=db)
+        account = _account_for_test(db=db)
 
         msg4_data = (4, set([r'\Seen']), "Subject: test message")
         msg22_data = (22, set([r'\Seen', r'\Answered']), "Subject: blah")
@@ -244,7 +244,7 @@ class PersistenceTest(unittest.TestCase):
         with mock_worker(myfolder={4: msg4_data, 22: msg22_data}) as worker:
             account.perform_update()
 
-        account2 = account_for_test(db=db)
+        account2 = _account_for_test(db=db)
         myfolder = account2.get_folder('myfolder')
         messages = list(myfolder.list_messages())
         messages.sort(key=lambda m: m.uid)
@@ -260,31 +260,31 @@ class PersistenceTest(unittest.TestCase):
 
     def test_message_removed(self):
         db = _make_test_db()
-        account = account_for_test(db=db)
+        account = _account_for_test(db=db)
         with mock_worker(fol1={6: None, 8: None}):
             account.perform_update()
 
         with mock_worker(fol1={6: None}):
             account.perform_update()
 
-        account2 = account_for_test(db=db)
+        account2 = _account_for_test(db=db)
         fol1 = account2.get_folder('fol1')
         self.assertEqual([m.uid for m in fol1.list_messages()], [6])
 
     def test_uidvalidity(self):
         db = _make_test_db()
-        account = account_for_test(db=db)
+        account = _account_for_test(db=db)
         msg13_data = (13, set([r'\Seen']), "Subject: test message")
         with mock_worker(fol1={13: msg13_data, 'UIDVALIDITY': 1234}):
             account.perform_update()
 
-        account2 = account_for_test(db=db)
+        account2 = _account_for_test(db=db)
         fol1 = account2.get_folder('fol1')
         self.assertEqual(fol1._uidvalidity, 1234)
 
     def test_uidvalidity_changed(self):
         db = _make_test_db()
-        account = account_for_test(db=db)
+        account = _account_for_test(db=db)
         msg13_data = (13, set([r'\Seen']), "Subject: test message")
         msg13_bis_data = (13, set([r'\Seen']), "Subject: another message")
         with mock_worker(fol1={13: msg13_data, 'UIDVALIDITY': 1234}):
@@ -292,7 +292,7 @@ class PersistenceTest(unittest.TestCase):
         with mock_worker(fol1={13: msg13_bis_data, 'UIDVALIDITY': 1239}):
             account.perform_update()
 
-        account2 = account_for_test(db=db)
+        account2 = _account_for_test(db=db)
         fol1 = account2.get_folder('fol1')
         self.assertEqual(fol1._uidvalidity, 1239)
         self.assertEqual([m.raw_headers for m in fol1.list_messages()],
