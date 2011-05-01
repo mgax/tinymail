23a922b86c92e3e1d230826ecb3a0c9efd117470
Alex Morega <alex@grep.ro>
2011-05-01 11:26:58 +0300

    split callback creation, to be reused with NSTimer

--- src/tinymail/async_cocoa.py
+++ src/tinymail/async_cocoa.py
@@ -10,12 +10,14 @@ class MainThreadHelper(NSObject):
         except:
             log.exception("Error in callback on mail thread")
 
-def call_on_main_thread(callback, *args, **kwargs):
+def object_and_selector_for_callback(callback, *args, **kwargs):
     pool = NSAutoreleasePool.new()
-
     obj = MainThreadHelper.new()
     obj.func = lambda: callback(*args, **kwargs)
-
     selector = objc.selector(obj.onMainThread, signature='v@:')
-    later = obj.performSelectorOnMainThread_withObject_waitUntilDone_
-    later(selector, None, False)
+    return obj, selector
+
+def call_on_main_thread(callback, *args, **kwargs):
+    obj, selector = object_and_selector_for_callback(callback, *args, **kwargs)
+    obj_later = obj.performSelectorOnMainThread_withObject_waitUntilDone_
+    obj_later(selector, None, False)
