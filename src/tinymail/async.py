fed237bc812023e824150444bd0fbe0c7f009103
Alex Morega <alex@grep.ro>
2011-05-05 23:20:38 +0300

    simple worker queue

--- src/tinymail/async.py
+++ src/tinymail/async.py
@@ -1,6 +1,7 @@
 import Queue
 import threading
 import logging
+from collections import deque
 import monocle
 
 try:
@@ -65,3 +66,51 @@ def start_worker(worker):
     thread = threading.Thread(target=worker_loop, args=(in_queue, worker))
     thread.start()
     return AsyncWorkerProxy(in_queue, thread)
+
+class SimpleWorkerManager(object):
+    """
+    A worker manager that allows at most one worker, and keeps a queue of
+    clients waiting for a worker. Essentially this is a mutex: only one client
+    can run at a time.
+    """
+
+    def __init__(self, create_worker, destroy_worker):
+        self._create_worker = create_worker
+        self._desotry_worker = destroy_worker
+        self._worker_busy = None
+        self._client_queue = deque()
+
+    def get_worker(self):
+        """
+        Get a worker. This is called by o-routines and they expect a Callback
+        in return.
+        """
+        cb = monocle.callback.Callback()
+        self._client_queue.append(cb)
+        self._dispatch_workers()
+        return cb
+
+    def _dispatch_workers(self):
+        # Create new workers if we can. Actually, since we can only have one
+        # worker, the logic is quite simple.
+        if self._worker_busy:
+            return
+
+        if not self._client_queue:
+            return
+
+        self._worker_busy = True
+
+        # the factory returns a callback; chain it to our client's result.
+        create_worker_cb = self._create_worker()
+        cb = self._client_queue.popleft()
+        create_worker_cb.add(cb)
+
+    def hand_back_worker(self, worker):
+        """
+        The client is done and no longer needs the worker.
+        """
+        destroy_worker_cb = self._desotry_worker(worker)
+        self._worker_busy = False
+        self._dispatch_workers()
+        return destroy_worker_cb
--- src/tinymail/tests/test_async.py
+++ src/tinymail/tests/test_async.py
@@ -1,6 +1,6 @@
 import unittest2 as unittest
 import monocle
-from mock import patch
+from mock import Mock, patch
 
 @monocle.o
 def add_one(value):
@@ -84,3 +84,74 @@ class AsyncWorkerTest(unittest.TestCase):
 
         self.assertTrue(isinstance(d.result, ValueError))
         self.assertEqual(d.result.args, ('hi',))
+
+class SimpleWorkerManagerTest(unittest.TestCase):
+    def setUp(self):
+        from tinymail.async import SimpleWorkerManager
+        self.create_worker = Mock()
+        self.destroy_worker = Mock()
+        self.wm = SimpleWorkerManager(self.create_worker, self.destroy_worker)
+
+    def test_get_one_worker(self):
+        worker = object()
+        self.create_worker.return_value = monocle.callback.defer(worker)
+        results = []
+        self.wm.get_worker().add(results.append)
+
+        self.assertEqual(results, [worker])
+
+    def test_worker_factory_delayed(self):
+        self.create_worker.return_value = monocle.callback.Callback()
+        worker_cb = self.wm.get_worker()
+        results = []
+        worker_cb.add(results.append)
+
+        # worker not ready, we should have received nothing
+        self.assertEqual(results, [])
+
+        worker = object()
+        create_worker_cb = self.create_worker.return_value
+        create_worker_cb(worker)
+
+        # worker ready, we should have received it
+        self.assertEqual(results, [worker])
+
+    def test_worker_done(self):
+        self.create_worker.return_value = monocle.callback.defer(object())
+        results = []
+        self.wm.get_worker().add(results.append)
+        worker = results[0]
+
+        self.wm.hand_back_worker(worker)
+
+        self.destroy_worker.assert_called_once_with(worker)
+
+    def test_request_sequence(self):
+        self.create_worker.return_value = monocle.callback.defer(object())
+        results = []
+        self.wm.get_worker().add(results.append)
+        worker = results[0]
+        self.wm.hand_back_worker(worker)
+
+        worker2 = object()
+        self.create_worker.return_value = monocle.callback.defer(worker2)
+        results2 = []
+        self.wm.get_worker().add(results2.append)
+
+        self.assertIs(results2[0], worker2)
+
+    def test_request_queue(self):
+        results1 = []; worker1 = object()
+        results2 = []; worker2 = object()
+        self.create_worker.return_value = monocle.callback.defer(worker1)
+        self.wm.get_worker().add(results1.append)
+        self.wm.get_worker().add(results2.append)
+
+        self.assertEqual(results1, [worker1])
+        self.assertEqual(results2, [])
+
+        self.create_worker.return_value = monocle.callback.defer(worker2)
+        self.wm.hand_back_worker(worker1)
+
+        self.assertEqual(results1, [worker1])
+        self.assertEqual(results2, [worker2])
